b4c736c83a661a668b9eb7758ea08d47dc08939e
Vyacheslav Onuchin <sla651@mail.ru>
2023-03-04 19:09:53 +1000

    Created file NetworkService.

--- JSONDecode.playground/Sources/NetworkService.swift
+++ JSONDecode.playground/Sources/NetworkService.swift
@@ -1,2 +1,12 @@
 import Foundation
 
+public struct queryResult: Decodable {
+    var cards: [Card]
+}
+
+public struct Card: Decodable {
+    let name: String
+    let manaCost: String?
+    let type: String
+    let setName: String
+}
